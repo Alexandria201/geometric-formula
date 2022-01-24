f3c691e1fc48a3b876ede0f5747f90d1abfe9ed9
Alexandria201 <aov6lt@ukr.net>
2022-01-24 12:20:03 +0200

    This is a test commit for npm

---
+++  libraryFormula.js
@@ -0,0 +1,15 @@
+function areaSquare(side) {
+    return side * side;
+}
+
+function areaRectangle(length, width) {
+    return length * width;
+}
+
+function areaParallelogram(base, height) {
+    return base * height;
+}
+
+function areaRhombus(base, height) {
+    return base * height;
+}
