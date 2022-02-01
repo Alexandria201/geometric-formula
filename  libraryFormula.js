94057afb626400e211db76c964759396bc4f9778
Alexandria201 <aov6lt@ukr.net>
2022-02-01 09:21:52 +0200

    Fix errors with linter

---  libraryFormula.js
+++  libraryFormula.js
@@ -1,15 +1,19 @@
+// eslint-disable-next-line no-unused-vars
 function areaSquare(side) {
     return side * side;
 }
 
+// eslint-disable-next-line no-unused-vars
 function areaRectangle(length, width) {
     return length * width;
 }
 
+// eslint-disable-next-line no-unused-vars
 function areaParallelogram(base, height) {
     return base * height;
 }
 
+// eslint-disable-next-line no-unused-vars
 function areaRhombus(base, height) {
     return base * height;
 }
--- domOperations.js
+++ domOperations.js
@@ -1,20 +1,27 @@
+// eslint-disable-next-line no-unused-vars
 function showResultInDomKvadrat() {
     const squareSide = document.getElementById('dlinaStoroniKvadrata').value;
+    // eslint-disable-next-line no-undef
     document.getElementById('ploschadKvadrata').innerHTML = areaSquare(squareSide);
 }
 
+// eslint-disable-next-line no-unused-vars
 function showResultInDomPriamougolnika() {
     const firstSide = document.getElementById('pervayaStoronaPriamougolnika').value;
     const secondSide = document.getElementById('drugayaStoronaPriamougolnika').value;
+    // eslint-disable-next-line no-undef
     document.getElementById('ploschadPriamougolnika').innerHTML = areaRectangle(firstSide, secondSide);
 }
 
+// eslint-disable-next-line no-unused-vars
 function showResultInDomParalelograma() {
     const sideLength = document.getElementById('dlinaStoronyParalelograma').value;
     const heightLength = document.getElementById('dlinaVisotyParalelograma').value;
+    // eslint-disable-next-line no-undef
     document.getElementById('ploschadParalelograma').innerHTML = areaParallelogram(sideLength, heightLength);
 }
 
+// eslint-disable-next-line no-unused-vars
 function showResultInDomRomba() {
     const baseRhombus = document.getElementById('dlinaStoronyRomba').value;
     const heightRhombus = document.getElementById('dlinaVisotyRomba').value;
